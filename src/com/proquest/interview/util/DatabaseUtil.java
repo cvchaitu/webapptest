bc5c2c8c02850dcb5dbb382f27c82afc75251c0d
cvchaitu <cvchaitanyareddy@gmail.com>
2015-10-15 21:57:37 -0500

    Update DatabaseUtil.java

--- src/com/proquest/interview/util/DatabaseUtil.java
+++ src/com/proquest/interview/util/DatabaseUtil.java
@@ -1,10 +1,3 @@
-package com.proquest.interview.util;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 /**
  * This class is just a utility class, you should not have to change anything here
  * @author rconklin
@@ -23,9 +16,85 @@ public class DatabaseUtil {
 			ex.printStackTrace();
 		}
 		
+		
 	}
+	
+	/**
+	 * Method inserts Person data into Database
+	 * @param Person
+	 * @return void
+	 * @throws Exception
+	 */
+	public static void insertPerson(Person person) {
+		try {
+		Connection cn = getConnection();
+		Statement stmt = cn.createStatement();
+		String insertQuery= "INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) VALUES('"+person.name+"','"+person.phoneNumber+"', '"+person.address+"')";
+		stmt.execute(insertQuery);
+	}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		}
+			
+	/**
+	 * Method finds person in Database and retrieve data
+	 * @param Person
+	 * @return void
+	 * @throws Exception
+	 */
+	public static Person findPerson(Person person) {
+		Person returnPerson = new Person();
+		try {
+		Connection cn = getConnection();
+		Statement stmt = cn.createStatement();
+				
+		String selectQuery= "SELECT * FROM PHONEBOOK WHERE NAME ='"+person.name+"';";
+		ResultSet results=  stmt.executeQuery(selectQuery);
+		while(results.next()){
+			returnPerson.name=results.getString("NAME");
+			returnPerson.phoneNumber=results.getString("PHONENUMBER");
+			returnPerson.address=results.getString("ADDRESS");
+				
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return returnPerson;
+	}
+			
+	/**
+	 * Method  retrieve all data from PhoneBook table
+	 * @param Person
+	 * @return void
+	 * @throws Exception
+	 */
+	public static List getPhoneBook() {
+		
+		List phoneBook = new ArrayList();
+		Person returnPerson = new Person();
+		
+		try {
+		Connection cn = getConnection();
+		Statement stmt = cn.createStatement();
+				
+		String selectQuery= "SELECT * FROM PHONEBOOK;";
+		ResultSet results=  stmt.executeQuery(selectQuery);
+		while(results.next()){
+			returnPerson.name=results.getString("NAME");
+			returnPerson.phoneNumber=results.getString("PHONENUMBER");
+			returnPerson.address=results.getString("ADDRESS");
+			phoneBook.add(returnPerson);
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return phoneBook;
+	}		
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		Class.forName("org.hsqldb.jdbcDriver");
 		return DriverManager.getConnection("jdbc:hsqldb:mem", "sa", "");
 	}
-}
+}                                                   
