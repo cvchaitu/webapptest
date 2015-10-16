ddf363b336566939c7216a9b606dfd5b45aa912a
cvchaitu <cvchaitanyareddy@gmail.com>
2015-10-15 21:55:19 -0500

    Update Person.java

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -1,6 +1,16 @@
 package com.proquest.interview.phonebook;
 
 public class Person {
+
+	public Person (String name, String phoneNumber,String address){
+		this.name = name;
+		this.phoneNumber= phoneNumber;
+		this.address=address;
+		
+	}
+	public Person() {
+		// TODO Auto-generated constructor stub
+	}
 	public String name;
 	public String phoneNumber;
 	public String address;
